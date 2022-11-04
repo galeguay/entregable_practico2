cd2cad21f85027ea91493f1706c7737a992121d2
Ale Guay <galeguay@gmail.com>
2022-11-04 16:59:44 -0300

    Se soluciono error de defasaje al clickear ficha

--- js/4enlinea.js
+++ js/4enlinea.js
@@ -15,43 +15,42 @@ let height = canvas.getBoundingClientRect().height;
 
 
 //Se crea una nueva instancia de la clase Tablero
-let tablero4 = new Tablero(4, "images/4enLinea/tablero4.png", 0, 0);
+let tablero4 = new Tablero(4, "images/4enLinea/tablero4.webp", 0, 0);
+tablero4.draw(ctx);
 
-//Creo una nueva instancia de la clase Ficha
+//Creo una nuevas instancias de la clase Ficha
 let fichaMessi = new Ficha("images/4enLinea/fichaMessi.png", "#FF0000", 10, 10, 50, 50, widht, height);
 let fichaRonaldo = new Ficha("images/4enLinea/fichaRonaldo.png", "red", 150, 10, 50, 50,  widht, height);
-
-tablero4.draw(ctx);
 fichaMessi.draw(ctx);
 fichaRonaldo.draw(ctx);
-
-
 
 /**Obtiene la posicion del cursor*/
 function getCursorPosition(canvas, event) {
   const rect = canvas.getBoundingClientRect();
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
-  //console.log("x: " + x + " y: " + y);
 }
 
 
 let isMouseDown = false;
 let fichaParaMover;
+let difX;
+let difY;
 
-/**Cuando se clickea el mouse */
+/**Cuando se clickea el mouse*/
 function onMouseDown(event){
   isMouseDown = true;
   const rect = canvas.getBoundingClientRect();
   const x = event.clientX - rect.left; //coordenadas x e y dentro del canvas
   const y = event.clientY - rect.top;
 
-  let clickFicha = fichaClickeada(x, y); 
-  if(clickFicha != null){
+  let clickFicha = fichaClickeada(x, y);
+  if(fichaClickeada(x, y) != null){
     fichaParaMover = clickFicha;
+    difX = x - fichaParaMover.getPosX();
+    difY = y - fichaParaMover.getPosY();
   }
 }
-
 /**Cuando se mueve el mouse mientras se esta clickeando*/
 function onMoveMouse(event){ 
   if(isMouseDown && fichaParaMover != null){
@@ -59,18 +58,16 @@ function onMoveMouse(event){
     let posX = event.clientX - rect.left;
     let posY = event.clientY - rect.top;
     if(fichaParaMover != null){
-      fichaParaMover.setPosition(posX, posY);
+      fichaParaMover.setPosition(posX - difX, posY - difY);
       fichaParaMover.draw(ctx);
     }
-    
   }
 }
-
-function onMouseUp(event){ // Mouse levantado
+/**Cuando se desclickea el mouse*/
+function onMouseUp(event){
   isMouseDown = false;
   fichaParaMover = null;
 }
-
 function fichaClickeada(x, y){
   if(fichaMessi.isPointInside(x, y)) return fichaMessi;
   else if(fichaRonaldo.isPointInside(x, y)) return fichaRonaldo;
