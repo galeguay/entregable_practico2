b32c4c49b419ee7a7e600250043ea10a6f4d4c3a
Ale Guay <galeguay@gmail.com>
2022-11-04 10:23:42 -0300

    Se corrigio metodo isPointInside de la clase Ficha

--- js/4enlinea.js
+++ js/4enlinea.js
@@ -1,75 +1,43 @@
 "use strict";
+/** @type {CanvasRenderingContext2D} */
 
 window.addEventListener('load', () => {
   document.addEventListener('mousedown', onMouseDown); // Inicia arrastrada
   document.addEventListener('mouseup', onMouseUp); // Detiene arrastrada
-  //document.addEventListener('mousemove', onMoveMouse); // Movimiento del mouse 
-
 })
 
 let canvas = document.querySelector('#gameCanvas');
-/** @type {CanvasRenderingContext2D} */
 let ctx = canvas.getContext('2d');
 
-
-//Creo una nueva instancia de la clase Tablero
+//Se crea una nueva instancia de la clase Tablero
 let tablero4 = new Tablero(4, "images/4enLinea/tablero4.png", 0, 0);
 tablero4.draw(ctx);
-//Creo una nueva instancia de la clase Ficha
+
+//Se crea una nueva instancia de la clase Ficha
 let fichaMessi = new Ficha("images/4enLinea/fichaMessi.png", "#FF0000", 10, 10, 50, 50);
 let fichaRonaldo = new Ficha("images/4enLinea/fichaRonaldo.png", "red", 150, 10, 50, 50);
 
 fichaMessi.draw(ctx);
 fichaRonaldo.draw(ctx);
 
-
-// function drawFicha(fichaMessi, fichaRonaldo){
-//   if(fichaMessi != null){
-//     fichaMessi.draw(ctx);
-//   }
-//   else if(fichaRonaldo != null)
-//   fichaRonaldo.draw(ctx);
-// }
-
 let isMouseDown = false;
-//Distintos eventos
+
 function onMouseDown(event){ // Mouse apretado
   isMouseDown = true;
-  let x = event.layerX;
-  let y = event.layerY;
-  
-  let clickFicha = fichaClickeada(x, y); //coordenadas x e y dentro del canvas
+  const rect = canvas.getBoundingClientRect()
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
   console.log(x, y);
-  if(clickFicha != null){
-    console.log(clickFicha);
-  }
-  
-  console.log("Se clickeo");
-  //drawGame();
+  console.log(fichaClickeada(x,y));
 }
 
 function fichaClickeada(x, y){
-  if(fichaMessi.isPointInside(x, y)) return "fichaMessi";
-  else if(fichaRonaldo.isPointInside(x, y)) return "fichaRonaldo";
+  if(fichaMessi.isPointInside(x, y))
+    return "fichaMessi";
+  else if(fichaRonaldo.isPointInside(x, y)) 
+    return "fichaRonaldo";
 }
-function onMouseUp(event){ // Mouse levantado
+
+function onMouseUp(event){
   isMouseDown = false;
 }
-
-// function onMoveMouse(event){ // Movimiento del mouse
-//   if(isMouseDown && fichaClickeada != null){
-//     fichaClickeada.setPosition(event.layerX, event.layerY);
-//   }
-//   //drawTablero();
-
-// }
-
-// function clearCanvas(){
-//   drawFicha();
-// }
-
-// function getMousePos(event){
-//   let cursorX = Math.round(event.layerX);
-//   let cursorY = Math.round(event.layerY);
-  
-// }
--- js/lib/Ficha.js
+++ js/lib/Ficha.js
@@ -1,36 +1,35 @@
 class Ficha {
     constructor(imagenSrc, colorBorde, posX, posY, widht, height){
-        this.imagenSrc = imagenSrc;
+        //this.imagenSrc = imagenSrc;
         this.colorBorde = colorBorde;
         this.posX = posX;
         this.posY = posY;
         this.widht = widht;
         this.height = height;
+        this.imagen = new Image()
+        this.imagen.src = imagenSrc;
     }
 
     draw(ctx){
         // clearCanvas(); //Limpia el canvas para despues 
-        let imagen = new Image();
-        imagen.src = this.imagenSrc;
+        let imagen1 = this.imagen;
         let x = this.posX;
         let y = this.posY;
         let widht = this.widht;
         let height = this.height;
-        imagen.onload = function () {
-            ctx.drawImage(imagen, x, y, widht, height);
+        imagen1.onload = function () {
+            ctx.drawImage(imagen1, x, y, widht, height);
             ctx.globalCompositeOperation = "source-atop";
             // ctx.globalCompositeOperation = "destination-over";
         };
         // this.ctx.strokeStyle = this.colorBorde;
         // this.ctx.lineWidht = 10;
         // this.ctx.stroke();
-        
     }
 
     setPosition(){ //Actualiza la posicion al mover el mouse
         this.posX = x;
         this.getPosY = y;
-        
     }
 
     getPosicion(){ //Obtiene la posicion actual
@@ -48,12 +47,12 @@ class Ficha {
         return this.posY;
     }
 
-    isPointInside(x, y){ //Ver si el puntero esta dentro de la ficha
-        let _x = this.posX - x; //La pos del circulo menos la pos del mouse
-        let _y = this.posY - y;
-        return Math.sqrt(_x * _x + _y * _y) < 50;
+    /**Chequea si el puntero esta dentro de la ficha */
+    isPointInside(cursorX, cursorY){
+        console.log(cursorX+" "+cursorY+"\n"+this.posX+" "+this.posY);
+        let _x = this.posX - cursorX;
+        let _y = this.posY - cursorY;
+        return Math.sqrt(_x * _x + _y * _y) < 60;
     }
-
-
 
 }
