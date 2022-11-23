ac718e603800b02a2ba97bcb060a46a5f36e1049
Ale Guay <galeguay@gmail.com>
2022-11-23 10:53:51 -0300

    Se modifico la clase Juego

--- js/4enLinea.js
+++ js/4enLinea.js
@@ -1,45 +1,12 @@
 "use strict";
 /** @type {CanvasRenderingContext2D} */
 
-window.addEventListener('load', function(){
-  document.getElementById('timer').style.display = "none";
-  let button = document.querySelector('#button4');
-  button = document.querySelector('#button4');
-  /*button = document.querySelector('#button5');
-  button = document.querySelector('#button6');
-  button = document.querySelector('#button7');*/
-  button.addEventListener('click', () =>{
-    document.querySelector('#titulo').style.display = "none";
-    document.getElementById('button4').style.display = "none";
-    document.getElementById('button5').style.display = "none";
-    document.getElementById('button6').style.display = "none";
-    document.getElementById('button7').style.display = "none";
-    document.getElementById('nombreJugador1').style.display = "none";
-    document.getElementById('nombreJugador2').style.display = "none";
-    document.getElementById('elegirFicha').style.display = "none";
-    document.getElementById('messi').style.display = "none";
-    document.getElementById('ronaldo').style.display = "none";
-    document.getElementById('elegirColor').style.display = "none";
-    document.getElementById('colorMessi').style.display = "none";
-    document.getElementById('colorRonaldo').style.display = "none";
-    document.getElementById('timer').style.display = "flex";
-  });
-  
-  document.addEventListener('mousedown', onMouseDown); // Inicia arrastrada
-  document.addEventListener('mouseup', onMouseUp); // Detiene arrastrada
-  document.addEventListener('mousemove', onMoveMouse); // Movimiento del mouse 
-  
-});
 
 /**Obtiene color elegido por el jugador */
 document.getElementById("colorMessi").addEventListener("input", function() {
   console.log(this.value);
 });
 
-let canvas = document.querySelector('#gameCanvas');
-let ctx = canvas.getContext('2d');
-let width = canvas.getBoundingClientRect().width;
-let height = canvas.getBoundingClientRect().height;
 let cantidadFichasParaGanar = 7;
 let timer = document.querySelector('#timer');
 let juego = new Juego(canvas, timer);
@@ -58,46 +25,4 @@ let difX;
 let difY;
 let fichaJugadorEsperando;
 
-/**Cuando se clickea el mouse*/
-function onMouseDown(event){
-  isMouseDown = true;
-  const rect = canvas.getBoundingClientRect();
-  const x = event.clientX - rect.left; //coordenadas x e y dentro del canvas
-  const y = event.clientY - rect.top;
-  fichaActiva = juego.getFichaActiva();
-  if (fichaActiva != null){
-    if (fichaActiva.isPointInside(x, y)){
-      fichaParaMover = fichaActiva;
-      difX = x - fichaParaMover.getX();
-      difY = y - fichaParaMover.getY();
-    }
-  }
-}
 
-/**Cuando se mueve el mouse mientras se esta clickeando*/
-function onMoveMouse(event){
-  if(isMouseDown && fichaParaMover != null){
-    const rect = canvas.getBoundingClientRect();
-    let posX = event.clientX - rect.left;
-    let posY = event.clientY - rect.top;
-    if(fichaParaMover != null){
-      fichaActiva.setPosition(posX - difX, posY - difY);
-      juego.clearCanvas();
-      fichaActiva.draw(ctx);
-    }
-  }
-}
-
-/**Cuando se desclickea el mouse*/
-function onMouseUp(event){
-  isMouseDown = false;
-  const rectt = canvas.getBoundingClientRect();
-  const xUpCursor = event.clientX - rectt.left; //coordenadas x e y dentro del canvas
-  const yUpCursor = event.clientY - rectt.top;
-  let columna = juego.getColumna(xUpCursor, yUpCursor);
-  if(columna != null && fichaParaMover != null){ //Si encuentra dropPoint y fichaParaMover
-    juego.clearCanvas(fichaParaMover);
-    juego.insertarFicha(fichaParaMover, columna);
-  }
-  fichaParaMover = null;
-}
--- js/lib/Juego.js
+++ js/lib/Juego.js
@@ -1,32 +1,110 @@
 class Juego {
-    constructor(canvas, timer) {
+    constructor(canvas) {
+        console.log("constructorJuego()");
         this.canvas = canvas;
         this.ctx = this.canvas.getContext("2d");
         this.width = this.canvas.getBoundingClientRect().width;
         this.height = this.canvas.getBoundingClientRect().height;
         this.partida;
-        this.timer = timer;
+        this.timer = document.querySelector('#timer');
         this.currentX;
         this.currentY;
         this.defaultXFichaJ1 = 70;
         this.defaultXFichaJ2 = 773;
         this.defaultYFichas = 175;
-
-        this.configurarPartida();
+        this.cargarInterfazDeAjustesDePartida();
+        //this.configurarPartida();
         //this.mouseEvents();
     }
 
-    configurarPartida(){
-        let jugador1 = new Jugador("Jugador1", "#FFFFFF", new Ficha(this.ctx,"images/4enLinea/fichaMessi.png", "#FF0000", this.defaultXFichaJ1, this.defaultYFichas, 50, 50));
-        let jugador2 = new Jugador("Jugador2", "#FFFFFF", new Ficha(this.ctx,"images/4enLinea/fichaRonaldo.png", "#FF0000", this.defaultXFichaJ2, this.defaultYFichas, 50, 50));
-        this.cargarPartida4enLinea(jugador1, jugador2);
+    cargarInterfazDeAjustesDePartida(){
+        console.log("cargarInterfazDeAjustesDePartida()");
+        window.addEventListener('load', function(){
+            document.getElementById('timer').style.display = "none";
+            let button = document.querySelector('#button4');
+            button = document.querySelector('#button4');
+            /*button = document.querySelector('#button5');
+            button = document.querySelector('#button6');
+            button = document.querySelector('#button7');*/
+            button.addEventListener('click', () =>{
+                document.querySelector('#titulo').style.display = "none";
+                document.getElementById('button4').style.display = "none";
+                document.getElementById('button5').style.display = "none";
+                document.getElementById('button6').style.display = "none";
+                document.getElementById('button7').style.display = "none";
+                document.getElementById('nombreJugador1').style.display = "none";
+                document.getElementById('nombreJugador2').style.display = "none";
+                document.getElementById('elegirFicha').style.display = "none";
+                document.getElementById('messi').style.display = "none";
+                document.getElementById('ronaldo').style.display = "none";
+                document.getElementById('elegirColor').style.display = "none";
+                document.getElementById('colorMessi').style.display = "none";
+                document.getElementById('colorRonaldo').style.display = "none";
+                document.getElementById('timer').style.display = "flex";
+            });
+            document.addEventListener('mousedown', this.onMouseDown); // Inicia arrastrada
+            document.addEventListener('mouseup', this.onMouseUp); // Detiene arrastrada
+            document.addEventListener('mousemove', this.onMoveMouse); // Movimiento del mouse 
+        });
     }
 
-    cargarPartida4enLinea(jugador1, jugador2) {
-        this.partida = new Partida(this.ctx, 4, 5, 30, jugador1, jugador2, this.timer);
+/**Cuando se clickea el mouse*/
+    onMouseDown(event){
+    isMouseDown = true;
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left; //coordenadas x e y dentro del canvas
+    const y = event.clientY - rect.top;
+    fichaActiva = juego.getFichaActiva();
+    if (fichaActiva != null){
+        if (fichaActiva.isPointInside(x, y)){
+            fichaParaMover = fichaActiva;
+            difX = x - fichaParaMover.getX();
+            difY = y - fichaParaMover.getY();
+        }
+    }
+}
+
+  /**Cuando se mueve el mouse mientras se esta clickeando*/
+    onMoveMouse(event){
+        if(isMouseDown && fichaParaMover != null){
+        const rect = canvas.getBoundingClientRect();
+        let posX = event.clientX - rect.left;
+        let posY = event.clientY - rect.top;
+        if(fichaParaMover != null){
+            fichaActiva.setPosition(posX - difX, posY - difY);
+            juego.clearCanvas();
+            fichaActiva.draw(ctx);
+        }
+        }
+    }
+
+  /**Cuando se desclickea el mouse*/
+    onMouseUp(event){
+        isMouseDown = false;
+        const rectt = canvas.getBoundingClientRect();
+        const xUpCursor = event.clientX - rectt.left; //coordenadas x e y dentro del canvas
+        const yUpCursor = event.clientY - rectt.top;
+        let columna = juego.getColumna(xUpCursor, yUpCursor);
+        if(columna != null && fichaParaMover != null){ //Si encuentra dropPoint y fichaParaMover
+        juego.clearCanvas(fichaParaMover);
+        juego.insertarFicha(fichaParaMover, columna);
+        }
+        fichaParaMover = null;
+    }
+
+    cargarPartida(nombreJugador1, nombreJugador2, cantidadFichasParaGanar) {
+        console.log("cargarPartida4enLinea()");
+        if(nombreJugador1 == "")
+            nombreJugador1 = "Jugador1";
+        if(nombreJugador2 == "")
+            nombreJugador1 = "Jugador2";
+        let jugador1 = new Jugador(nombreJugador1, "#FFFFFF", new Ficha(this.ctx,"images/4enLinea/fichaMessi.png", "#FF0000", this.defaultXFichaJ1, this.defaultYFichas, 50, 50));
+        let jugador2 = new Jugador(nombreJugador2, "#FFFFFF", new Ficha(this.ctx,"images/4enLinea/fichaRonaldo.png", "#FF0000", this.defaultXFichaJ2, this.defaultYFichas, 50, 50));
+        this.partida = new Partida(this.ctx, cantidadFichasParaGanar, 5, 30, jugador1, jugador2, this.timer);
     }
 
     fichaClickeada(x, y) {
+        console.log("fichaClickeada()");
         this.partida.fichaClickeada();
         if (fichaMessi.isPointInside(x, y)) return fichaMessi;
         else if (fichaRonaldo.isPointInside(x, y)) return fichaRonaldo;
